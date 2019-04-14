64f4230d8c512b261b4b58b99615d39e24dca126
liberbell <liberbell@gmail.com>
2019-04-14 12:16:59 +0900

    views4

--- portfolio1/jobs/views.py
+++ portfolio1/jobs/views.py
@@ -1,8 +1,5 @@
 from django.shortcuts import render
 
 # Create your views here.
-def taro(request):
-    return render(request, 'jobs/taro.html')
-
 def homepage(request):
     return render(request, 'jobs/home.html')
