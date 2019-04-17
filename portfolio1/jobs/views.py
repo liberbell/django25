24bdf344558e61e470d16c4eae44756c2e18a906
liberbell <liberbell@gmail.com>
2019-04-18 08:01:43 +0900

    views2

--- portfolio1/jobs/views.py
+++ portfolio1/jobs/views.py
@@ -6,6 +6,6 @@ def homepage(request):
     jobs = Job.objects
     return render(request, 'jobs/home.html', {'jobs':jobs})
 
-    def detail(request):
-        jobs = Job.objects
-        return render(request, 'jobs/home.html', {'jobs':jobs})
+    def detail(request, job_id):
+        print(job_id)
+        return render(request, 'jobs/home.html')
